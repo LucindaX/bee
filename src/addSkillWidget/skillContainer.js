da2fcc348540a68370f3257b4021e000b765fee0
lucindax <ahmedosmanazrk@gmail.com>
2018-08-13 00:13:00 +0200

    removed onMouse events and replaced with css hover
    There is an open bug in Reacts onMouseLeave & onMouseEnter
    events where it doesn't register these events. Using css
    hover is the best reliable and effiecent option till its
    fixed.

--- src/addSkillWidget/skillContainer.js
+++ src/addSkillWidget/skillContainer.js
@@ -10,13 +10,6 @@ class SkillContainer extends Component {
       skills: this.props.skills,
       showDelete: []
     };
-    this.handleMouseHover = this.handleMouseHover.bind(this);
-  }
-
-  handleMouseHover(index){
-    let showDelete = this.state.showDelete.slice(0);
-    showDelete[index] = !showDelete[index];
-    this.setState({ showDelete: showDelete });
   }
 
   componentWillReceiveProps(nextProps){
@@ -25,13 +18,10 @@ class SkillContainer extends Component {
 
   render(){
     const list = this.state.skills.map( (ele, index) => 
-      
-        <div className="skill-box col-12 col-md-3"
-        key={index}>
 
-        <div className="row no-gutters" 
-          onMouseEnter={this.handleMouseHover.bind(this,index)}
-          onMouseLeave={this.handleMouseHover.bind(this,index)}>
+        <div className="skill-box col-12 col-md-3" key={index}>
+
+        <div className="skill-box-row row no-gutters" >
          
           <div className="index-badge col-1">{index+1}</div>
           
@@ -40,7 +30,7 @@ class SkillContainer extends Component {
             {ele.experience}
           </div>
           
-          <div className={ 'close-tag col-2 offset-1 '+ (this.state.showDelete[index] ? '' : 'd-none') }>
+          <div className='close-tag col-2 offset-1'>
             <FontAwesomeIcon icon={faTimes} />
           </div>
 
